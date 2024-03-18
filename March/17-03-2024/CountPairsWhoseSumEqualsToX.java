dec69379f11c87bd437cfb30acd687380817a0ad
Kumar Prince <princekumarkolkata18@gmail.com>
2024-03-18 19:52:45 +0530

    Count Pairs Whose Sum Equals To X GFG POTD 17-03-2024 Problem Solution

--- March/17-03-2024/CountPairsWhoseSumEqualsToX.java
+++ March/17-03-2024/CountPairsWhoseSumEqualsToX.java
@@ -17,54 +17,53 @@ public class CountPairsWhoseSumEqualsToX {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-            int n1 = sc.nextInt();
-            Integer arr1[] = new Integer[n1];
+        int n1 = sc.nextInt();
+        Integer arr1[] = new Integer[n1];
 
-            for (int i = 0; i < n1; i++) arr1[i] = sc.nextInt();
+        for (int i = 0; i < n1; i++)
+            arr1[i] = sc.nextInt();
 
-            int n2 = sc.nextInt();
-            Integer arr2[] = new Integer[n2];
+        int n2 = sc.nextInt();
+        Integer arr2[] = new Integer[n2];
 
-            for (int i = 0; i < n2; i++) arr2[i] = sc.nextInt();
+        for (int i = 0; i < n2; i++)
+            arr2[i] = sc.nextInt();
 
-            LinkedList<Integer> head1 = new LinkedList<>(Arrays.asList(arr1));
-            LinkedList<Integer> head2 = new LinkedList<>(Arrays.asList(arr2));
+        LinkedList<Integer> head1 = new LinkedList<>(Arrays.asList(arr1));
+        LinkedList<Integer> head2 = new LinkedList<>(Arrays.asList(arr2));
 
-            int x = sc.nextInt();
-            Solution gfg = new Solution();
-            System.out.println(gfg.countPairs(head1, head2, x));
-        
+        int x = sc.nextInt();
+        Solution gfg = new Solution();
+        System.out.println(gfg.countPairs(head1, head2, x));
+
     }
 }
-
-
-
 
 // your task is to complete this function
 
 /*
-class Node
-{
-    int data;
-    Node next;
-
-    Node(int key)
-    {
-        data = key;
-        next = null;
-    }
-}
-*/
+ * class Node
+ * {
+ * int data;
+ * Node next;
+ * 
+ * Node(int key)
+ * {
+ * data = key;
+ * next = null;
+ * }
+ * }
+ */
 
 class Solution {
 
     public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
-                          int x) {
+            int x) {
         // add your code here
-        int count=0;
+        int count = 0;
         HashSet<Integer> temp = new HashSet<>(head1);
-        for(Integer nums : head2){
-            if(temp.contains(x-nums)){
+        for (Integer nums : head2) {
+            if (temp.contains(x - nums)) {
                 count++;
             }
         }
